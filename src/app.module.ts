6dfa1a8f44915f085870d1e72446c745123a8654
shair1020 <ali99shair@gmail.com>
2024-01-15 13:13:25 +0500

    feat : create login and signup api on nest js with mongo db database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { UsersModule } from "./users/users.module";
+import { AuthModule } from './auth/auth.module';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot(
+      "mongodb+srv://ali99shair:alirafi99@cluster0.60hzdra.mongodb.net/auth-learn?retryWrites=true&w=majority",
+    ),
+    UsersModule,
+    AuthModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
+
